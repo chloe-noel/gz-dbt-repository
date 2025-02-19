9608bcf374eaeef584323c686c26d3e6e181679a
NOEL CHLOE <colhine.pole@gmail.com>
2025-02-19 17:04:13 +0000

    recommit

--- int_campaigns_day.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-date_date
-,SUM(COALESCE(ads_cost,0)) AS ads_cost
-,SUM(COALESCE(impression,0)) AS impression
-,SUM(COALESCE(click,O)) AS click
-FROM {{ ref('int_campaigns') }}
-GROUP BY date_date
-ORDER BY date_date DESC
